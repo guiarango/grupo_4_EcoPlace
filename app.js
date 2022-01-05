5592cd205a41d3d7096f41e2613438c1609c1493
Elias L <eliasilverg@gmail.com>
2022-01-04 22:51:13 -0300

    sass, se borra carpeta src, se trabaja en responsive

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const homeRoute = require("./routes/main");
 
 // Se deja abierta al público la carpeta public
-const publicPath = path.resolve(__dirname, "../public");
+const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
 // Se carga el servidor en el localhost:3000
