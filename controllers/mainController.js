a50e02aad8ec893d3cf774c12e84c542b00716d7
cocopestarino <pestarino35@gmail.com>
2022-01-13 13:29:14 -0300

    DB de products sin terminar

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,27 +1,37 @@
 const path = require("path");
+const fs = require("fs");
 
-const mainController = {
-  home: (req, res) => {
-    res.render("home");
-  },
-  carrito: (req, res) => {
-    res.render("users/carrito");
-  },
-  product: (req, res) => {
-    res.render("products/product_detail");
-  },
-  signIn: (req, res) => {
-    res.render("users/sign_in");
-  },
-  signUp: (req, res) => {
-    res.render("users/sign_up");
-  },
-  createProduct: (req, res) => {
-    res.render("products/products_create");
-  },
-  editProduct: (req, res) => {
-    res.render("products/products_edit");
-  },
-};
+const productFilePath = path.resolve(__dirname, "../data/DB-products.json");
+const products = JSON.parse(fs.readFileSync(productFilePath, "utf-8"));
+
+
+  const mainController = {
+    home: (req, res) => {
+      res.render("home");
+    },
+    carrito: (req, res) => {
+      res.render("users/carrito");
+    },
+    product: (req, res) => {
+      res.render("products/product_detail");
+    },
+    signIn: (req, res) => {
+      res.render("users/sign_in");
+    },
+    signUp: (req, res) => {
+      res.render("users/sign_up");
+    },
+    createProduct: (req, res) => {
+      res.render("products/products_create");
+    },
+    editProduct: (req, res) => {
+      res.render("products/products_edit");
+    },
+    productsList: (req, res) => {
+      return res.render("products", {
+        productsList: products,
+      });
+    }
+  };
 
 module.exports = mainController;
--- routes/main.js
+++ routes/main.js
@@ -9,5 +9,6 @@ router.get("/sign_in", mainController.signIn);
 router.get("/sign_up", mainController.signUp);
 router.get("/products_create", mainController.createProduct);
 router.get("/products_edit", mainController.editProduct);
+router.get("/products", mainController.productsList);
 
 module.exports = router;
