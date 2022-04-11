668fba6dcace9cb0462f84e88185014d63066a1d
Elias L <eliasilverg@gmail.com>
2022-04-11 17:46:13 -0400

    trabajando en validaciones front

--- public/js/validationSignUp.js
+++ public/js/validationSignUp.js
@@ -1,32 +1,79 @@
-window.addEventListener("load", function(){
-    let formulario = document.querySelector("form.signupForm")
-    formulario.addEventListener("submit", function(event) {
-        let errores = []
-        let usuario = document.querySelector("#user")
-         if (usuario.value == "") {
-             
-            errores.push("El campo de usuario no puede estar vacío")
-            
-         }
-         
-         let email = document.querySelector("#email")
-         if (email.value =="") {
-             errores.push ("El campo de email no puede estar vacío")
-             
-         }
 
-         let password = document.querySelector("#password")
-         if (password.value = "") {
-            errores.push ("El campo de password no puede estar vacío")
-         }
-         if (errores.length > 0 ) {
-                event.preventDefault();
-                let ulErrors = document.querySelector(".front-validation-error ul");
-                for (let i = 0; i < errores.length; i++) {
-            
-                    ulErrors.innerHTML += "<li>" +errores[i]+ "</li>"
-                }
+window.addEventListener("load", function(){
+    let signUpForm = document.querySelector("form.signupForm")
+    signUpForm.addEventListener("submit", function(event) {
+
+        let errorsCount = 0;
+        event.preventDefault();
+        const formFields = [...signUpForm.elements];
+        formFields.pop();
+        
+        formFields.forEach(oneField => {
+            const spanTagError = oneField.nextElementSibling;
+            if (oneField.value.trim() === "") {
+                spanTagError.classList.add("text-error");
+                spanTagError.innerText = "El campo es obligatorio";
+                errorsCount++;
+            } 
+            else {
+                spanTagError.classList.remove("text-error");
+                spanTagError.innerText = "";
             }
+        })
+        
+
+            if (errorsCount > 0) {
+                event.preventDefault();
+            }
+        
+
+
+
+
+
+
+
+        //PRIMERA LOGICA
+        // let errores = []
+        // let userSignUp = document.querySelector("#user")
+        //  if (userSignUp.value.trim() == "") {
+             
+        //     errores.push("El campo de usuario no puede estar vacío")
+            
+        //  }
+         
+        //  if (userSignUp.value.length == 1) {
+             
+        //     errores.push("El campo de usuario debe tener al menos 2 caracteres")
+            
+        //  }
+         
+        //  let emailSignUp = document.querySelector("#email")
+        //  if (emailSignUp.value.trim() == "") {
+        //      errores.push ("El campo de email no puede estar vacío")
+             
+        //  }
+
+        //  let passwordSignUp = document.querySelector("#password")
+        //  if (passwordSignUp.value.trim() == "") {
+        //     errores.push ("El campo de password no puede estar vacío")
+        //  }
+
+        //  if (passwordSignUp.value.length > 0 && passwordSignUp.value.length < 8) {
+        //     errores.push ("Su password debe tener al menos 8 caracteres")
+        //  }
+
+        //  let avatarSignUp = document.querySelector("#avatarImg")
+        //  console.log(event.value)
+
+        //  if (errores.length > 0 ) {
+        //         event.preventDefault();
+        //         let ulErrors = document.querySelector(".front-validation-error ul");
+        //         for (let i = 0; i < errores.length; i++) {
+            
+        //             ulErrors.innerHTML += "<li>" +errores[i]+ "</li>"
+        //         }
+        //     }
 
     })
 
